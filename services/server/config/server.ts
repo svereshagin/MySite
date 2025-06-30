fb8f833b83cd1d25f15a2006b4213f7e7068c930
amadeo <risefenixpuryfire@gmail.com>
2025-06-30 19:20:51 +0300

    strapi backend try

--- services/server/config/server.ts
+++ services/server/config/server.ts
@@ -1,8 +1,12 @@
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', 'https://strapi.proggyit.ru'),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  url: env('PUBLIC_URL', 'https://strapi.proggyit.ru'),
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  allowedHosts: ['strapi.proggyit.ru'], // 👈 добавь сюда
 });
