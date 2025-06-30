ffec6ef83b1bc357a379934ccdeddf3c25b55ac1
amadeo <risefenixpuryfire@gmail.com>
2025-06-30 10:35:30 +0300

    nginx file ready

--- services/server/types/generated/components.d.ts
+++ services/server/types/generated/components.d.ts
@@ -62,6 +62,17 @@ export interface SharedSlider extends Struct.ComponentSchema {
   };
 }
 
+export interface SkillsSkills extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'skills';
+  };
+  attributes: {
+    level: Schema.Attribute.String;
+    skill: Schema.Attribute.String;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
@@ -70,6 +81,7 @@ declare module '@strapi/strapi' {
       'shared.rich-text': SharedRichText;
       'shared.seo': SharedSeo;
       'shared.slider': SharedSlider;
+      'skills.skills': SkillsSkills;
     }
   }
 }
